2d056082df9502bd761db08ebb5c9cd23b952213
William Reeder <whhriv@gmail.com>
2023-09-30 08:53:51 -0700

    It works right here

--- shopping.py
+++ shopping.py
@@ -9,30 +9,26 @@ inventory = {
 
 def main():
     while True:
-        # ask = input('Pleast type quit to quit')
-        # if ask == 'quit':
-        #     break
-        # else:
-        #     continue
-        print('Welcome to Buenos Rancheros')
+        print('Welcome to Buenos Rancheros\n\n\n')
         print(inventory)
-        sel_option = input("Please type 'inventory' to see what we have in stock, 'mycart' to view your shopping cart, 'remove' to remove items or 'quit' to quit at any time")
-        
-        
+        sel_option = input("Please type 'inventory' to see what we have in stock, 'mycart' to view your shopping cart, 'remove' to remove items or 'quit' to quit at any time:  \n\n\n  : ")  
+        print("          :")
         if sel_option == 'inventory':
             print(inventory)  #add formating to style dict
-            to_buy = input('type item to add to cart: ')
+            to_buy = input('type item to add to cart\n :')
             cart[to_buy] = '1'
-            
-            
+            print("you have added" +' '+ to_buy + ' '+"to cart\n:")       
         elif sel_option == 'mycart':
             print(cart)
         elif sel_option == 'remove':
             print(cart)
-            del_item = input('please type name of item to remove: ')
-            removed = cart.pop(del_item)
-
+            del_item = input('please type name of item to remove:\n :')
+            if del_item not in cart:
+                print("You don't have that item")
+            else:
+                removed = cart.pop(del_item)
         elif sel_option == 'quit':
+            print(cart)
             break
         else:
             continue
